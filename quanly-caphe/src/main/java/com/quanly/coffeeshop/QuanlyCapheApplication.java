bcdeee5d356b13cd2acc87cc3021032fe87d7b90
taim34451-max <your_github_email@example.com>
2026-07-01 18:35:20 +0700

    Initial commit for demo1

---
+++ quanly-caphe/src/main/java/com/quanly/coffeeshop/QuanlyCapheApplication.java
@@ -0,0 +1,13 @@
+package com.quanly.coffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlyCapheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlyCapheApplication.class, args);
+	}
+
+}
---
+++ quanly-caphe/src/test/java/com/quanly/coffeeshop/QuanlyCapheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanly.coffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlyCapheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
